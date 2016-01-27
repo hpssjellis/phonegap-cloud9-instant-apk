9044cfa796aa89d97448a8064d450a44107269bf
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-26 20:02:29 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -130,39 +130,40 @@ android update sdk --filter android-23 --no-ui --force   #Android 6.0    Marshma
 echo "Android SDK setup"
 
 
-#echo "Now Gradle"
+echo "Now Gradle"
 
-#gradle_version=2.9
+gradle_version=2.9
 
-#mkdir /home/ubuntu/workspace/gradle
-#wget -N http://downloads.gradle.org/distributions/gradle-${gradle_version}-all.zip
-#unzip -oq ./gradle-${gradle_version}-all.zip -d /home/ubuntu/workspace/gradle
-#ln -sfnv gradle-${gradle_version} /home/ubuntu/workspace/gradle/gradle-${gradle_version}
+mkdir /home/ubuntu/workspace/gradle
+wget -N http://downloads.gradle.org/distributions/gradle-${gradle_version}-all.zip
+unzip -oq ./gradle-${gradle_version}-all.zip -d /home/ubuntu/workspace/gradle
+ln -sfnv gradle-${gradle_version} /home/ubuntu/workspace/gradle/gradle-${gradle_version}
 
-#echo "exporting the Path to my .profile file so other terminals have the path"
+echo "exporting the Path to my .profile file so other terminals have the path"
 
-#printf "\nexport GRADLE_HOME=/home/ubuntu/workspace/gradle/gradle-$gradle_version\nexport PATH=\$PATH:\$GRADLE_HOME/bin"  >> ~/.profile
-
-
-#echo "exporting the path so that the next command works"
+printf "\nexport GRADLE_HOME=/home/ubuntu/workspace/gradle/gradle-$gradle_version\nexport PATH=\$PATH:\$GRADLE_HOME/bin"  >> ~/.profile
 
 
-#export GRADLE_HOME=/home/ubuntu/workspace/gradle/gradle-$gradle_version
-#export PATH=$PATH:$GRADLE_HOME/bin
+echo "exporting the path so that the next command works"
+
+
+export GRADLE_HOME=/home/ubuntu/workspace/gradle/gradle-$gradle_version
+export PATH=$PATH:$GRADLE_HOME/bin
 
 
 #printf "export GRADLE_HOME=/home/ubuntu/workspace/gradle/latest\nexport PATH=\$PATH:\$GRADLE_HOME/bin" > /etc/profile.d/gradle.sh
 #. /etc/profile.d/gradle.sh
-#hash -r ; sync
+hash -r ; sync
 # check installation
 
-#echo "setup gradle"
+echo "setup gradle"
 
-#gradle
-#echo "see if it worked"
-#gradle -v
+gradle
+echo "see if it worked"
+gradle -v
 
-#rm gradle-2.9-all.zip
+rm gradle-2.9-all.zip
+
 
 echo "Now install Phonegap"
 
